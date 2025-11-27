d1b488af214b53bd0da77722e692b7c96b6fa0f4
BLACKBOX Agent <code@blackbox.ai>
2025-11-27 05:04:50 +0000

    refactor(ui): remove post screen

--- src/navigations/Navigator.js
+++ src/navigations/Navigator.js
@@ -1,12 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import { View, Image, Text, Platform } from 'react-native';
+import React from 'react';
+import { View, Image, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from '../screens/Home/Home';
 import Notifications from '../screens/Notifications/Notifications';
-import Post from '../screens/Post/Post';
-import Pic from '../screens/Post/Pic';
 import Profile from '../screens/Profile/Profile';
 import Search from '../screens/Search/Search';
 import Colors from '../utilities/Colors';
@@ -20,9 +18,6 @@ const TabNavigator = createBottomTabNavigator();
 
 const Navigator = ({ navigation, route }) => {
   const bottomnavigator = () => {
-    const [postTabBarHide, setPostTabBarhide] = useState(false);
-
-    useEffect(() => {}, [postTabBarHide]);
     //Bottom tab bar and its events
     return (
       <>
@@ -66,46 +61,6 @@ const Navigator = ({ navigation, route }) => {
           />
 
           <MaterialBottomTabNavigator.Screen
-            name="Post"
-            component={Post}
-            options={{
-              // drawBehind: false,
-              // bottomTabs: {
-              //     visible: false
-              // },
-              tabBarVisible: !postTabBarHide, //Show & hide tab bar
-              tabBarLabel: ({ focused }) => (
-                <Text
-                  style={{
-                    color: focused ? 'black' : Colors.themeLightGrayTextColor,
-                    fontSize: 12,
-                  }}
-                >
-                  Post
-                </Text>
-              ) /* Custom label for tab bar*/,
-
-              tabBarIcon: ({ focused }) => (
-                <Image
-                  source={Images.homePostIcon}
-                  style={{
-                    width: 24,
-                    height: 24,
-                    tintColor: focused
-                      ? Colors.themeYellowColor
-                      : Colors.themeLightGrayTextColor,
-                  }}
-                />
-              ) /* Custom icon for tab bar*/,
-            }}
-            listeners={() => ({
-              tabPress: e => {
-                setPostTabBarhide(true); // Setting Tab bar hideen is true for Post tab or screen.
-              },
-            })}
-          />
-
-          <MaterialBottomTabNavigator.Screen
             name="Search"
             component={Search}
             options={{
@@ -122,18 +77,7 @@ const Navigator = ({ navigation, route }) => {
               tabBarIcon: ({ focused }) => (
                 <>
                   {/* Seach Bottom tab custom component */}
-                  <View
-                    style={[
-                      Style.searchTabBar,
-                      {
-                        bottom: postTabBarHide
-                          ? Platform.OS === 'ios'
-                            ? 0
-                            : -25
-                          : 10,
-                      },
-                    ]}
-                  >
+                  <View style={[Style.searchTabBar, { bottom: 10 }]}>
                     <Image
                       source={Images.homeSearchIcon}
                       style={[
@@ -219,11 +163,6 @@ const Navigator = ({ navigation, route }) => {
         <Stack.Screen
           name="Home"
           component={bottomnavigator}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Pic"
-          component={Pic}
           options={{ headerShown: false }}
         />
       </Stack.Navigator>
